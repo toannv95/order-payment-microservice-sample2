c4046668f09a9cafee5e3a1c33e59e6a1719d220
Toan Nguyen <toan.nguyen@personifyinc.com>
2023-01-30 15:10:37 +0700

    update faker data

--- order-payment-microservice-sample/payment-service/src/main/java/org/toannguyen/models/Customer.java
+++ order-payment-microservice-sample/payment-service/src/main/java/org/toannguyen/models/Customer.java
@@ -16,7 +16,9 @@ public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
+    private String firstName;
+    private String lastName;
+    private String address;
     private int amountAvailable;
     private int amountReserved;
 }
--- order-payment-microservice-sample/stock-service/src/main/resources/schema.sql
+++ order-payment-microservice-sample/stock-service/src/main/resources/schema.sql
@@ -6,7 +6,9 @@ CREATE TABLE IF NOT EXISTS public.product
 (
     id serial CONSTRAINT product_pkey PRIMARY KEY,
     available_items integer NOT NULL,
+    code character varying(255) COLLATE pg_catalog."default",
     name character varying(255) COLLATE pg_catalog."default",
+    color character varying(255) COLLATE pg_catalog."default",
     reserved_items integer NOT NULL
 )
 
