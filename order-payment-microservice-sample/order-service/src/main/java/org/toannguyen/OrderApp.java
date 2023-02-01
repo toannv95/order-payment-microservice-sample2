3ab899f86a25c8cb7ccad1659c79a08f7f858cf4
Toan Nguyen <toan.nguyen@personifyinc.com>
2023-02-01 15:51:26 +0700

    fix service name in docker-compose.yml

--- order-payment-microservice-sample/order-service/src/main/java/org/toannguyen/OrderApp.java
+++ order-payment-microservice-sample/order-service/src/main/java/org/toannguyen/OrderApp.java
@@ -1,5 +1,6 @@
 package org.toannguyen;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.*;
